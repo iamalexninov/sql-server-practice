3643bd7366add5f6b97d7f97036bb40bbe1b191d
iamalexninov <iamalexgninov@gmail.com>
2025-02-13 20:28:37 +0200

    add procedure SearchMemberByName

---
+++ stored procedures/SearchMemberByName.sql
@@ -0,0 +1,23 @@
+/*
+SearchMemberByName
+
+Input: SearchString
+Task: Find members whose first or last name contains the search string
+Should show: MemberID, FullName, Email, MembershipType
+Practice: LIKE operator
+*/
+
+alter procedure SearchMemberByName
+	@SearchString nvarchar(10)
+as
+begin
+	select 
+		MemberID,
+		(FirstName + ' ' + LastName) as FullName,
+		Email,
+		MembershipType
+	from Members
+	where FirstName like '%' + @SearchString + '%' or 
+		  LastName like '%' + @SearchString + '%';
+end;
+
